d8284e5bc13f38c5fe171a726231c3721a8966da
sequeloble <tecnicaiplace@iPlaces-MacBook-Pro.local>
2017-07-27 09:55:06 -0300

    Soma entre numeros inteiros

---
+++ Desafios/003.py
@@ -0,0 +1,5 @@
+#Soma entre dois numeros inteiros
+n1 = float(input('Digite o primeiro numero: '))
+n2 = float(input('Digite o segundo: '))
+s = n1 + n2
+print('A soma entre {} e {} é igual {}.'.format(n1, n2, s))
