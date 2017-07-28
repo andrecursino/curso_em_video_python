d90153f20f3ff2a58f0daca0cf8b47d1121cea55
sequeloble <tecnicaiplace@iPlaces-MacBook-Pro.local>
2017-07-28 15:02:02 -0300

    tabuada

---
+++ Desafios/009.py
@@ -0,0 +1,22 @@
+#tabudada
+n = float(input('Insira o numero: '))
+n2 = n * 2
+n3 = n * 3
+n4 = n * 4
+n5 = n * 5
+n6 = n * 6
+n7 = n * 7
+n8 = n * 8
+n9 = n * 9
+n10 = n * 10
+print('Tabuada do {}:\n'.format(n),
+      '1 x {} = {}\n'.format(n, n),
+      '2 x {} = {}\n'.format(n, n2),
+      '3 x {} = {}\n'.format(n, n3),
+      '4 x {} = {}\n'.format(n, n4),
+      '5 x {} = {}\n'.format(n, n5),
+      '6 x {} = {}\n'.format(n, n6),
+      '7 x {} = {}\n'.format(n, n7),
+      '8 x {} = {}\n'.format(n, n8),
+      '9 x {} = {}\n'.format(n, n9),
+      '10 x {} = {}.'.format(n, n10))
