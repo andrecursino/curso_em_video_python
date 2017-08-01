b6eb19ae81036d0c76adfa9ba63427c88b79118c
sequeloble <tecnicaiplace@iPlaces-MacBook-Pro.local>
2017-08-01 15:54:23 -0300

    primeiro e ultimo nome

--- Desafios/027.py
+++ Desafios/027.py
@@ -1,4 +1,4 @@
 nome = str(input('Nome completo: '))
 nomediv = nome.split()
 print('Primeiro nome: {}'.format(nomediv[0]))
-print('Segundo nome: {}.'.format(nomedivgt[-1]))
+print('Segundo nome: {}.'.format(nomediv[-1]))
