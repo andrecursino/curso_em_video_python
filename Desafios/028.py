dbfde1fa279518f9939486d1569921ce6d296165
sequeloble <tecnicaiplace@iPlaces-MacBook-Pro.local>
2017-08-08 15:47:26 -0300

    Pensar entre 0 e 5

--- Desafios/028.py
+++ Desafios/028.py
@@ -1,7 +1,9 @@
 from random import randint
-num = int(input('Insira um número de 1 a 5: '))
-numpc = randint(1, 5)
-
+from time import sleep
+num = int(input('Insira um número de 0 a 5: '))
+numpc = randint(0, 5) #pc pensa entre 0 e 5
+print('PROCESSANDO...')
+sleep(3) #aguarda 3sec antes do proximo comando
 if num == numpc:
     print('Acertou Miseravi!')
 else:
