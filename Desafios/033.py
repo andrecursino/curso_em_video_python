dd4b6d304a251b9339331b326c49d1faaca804c6
sequeloble <tecnicaiplace@iPlaces-MacBook-Pro.local>
2017-08-16 15:44:42 -0300

    mostrar o > e o < num

--- Desafios/033.py
+++ Desafios/033.py
@@ -1,21 +1,17 @@
 n1 = int(input('Insira o primeiro número: '))
 n2 = int(input('Insira o segundo número: '))
 n3 = int(input('Insira o terceiro número: '))
-if n1 > n2 and n1 > n3:
-    print('{} é o numero maior.'.format(n1))
-    if n2 > n3:
-        print('{} é o menor numero.'.format(n3))
-    else:
-        print('{} é o menor número'.format(n2))
+# verificar o menor
+menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+# vericar o maior
+maior = n1
 if n2 > n1 and n2 > n3:
-    print('{} é o maior número.'.format(n2))
-    if n1 > n3:
-        print('{} é o menor número.'.format(n3))
-    else:
-        print('{} é o menor número.'.format(n1))
+    maior = n2
 if n3 > n1 and n3 > n2:
-    print('{} é o maior número.'.format(n3))
-    if n1 > n2:
-        print('{} é o menor número.'.format(n2))
-    else:
-        print('{} é o menor número.'.format(n1))
+    maior = n3
+print('O menor valor digitado foi {}.'.format(menor))
+print('O maior valor digitado foi {}.'.format(maior))
