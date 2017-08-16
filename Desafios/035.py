f66c6c607c29279259ea1abfaff00cb2d27eee35
sequeloble <tecnicaiplace@iPlaces-MacBook-Pro.local>
2017-08-16 15:45:06 -0300

    triangulo

--- Desafios/035.py
+++ Desafios/035.py
@@ -1,8 +1,7 @@
 r1 = float(input('Comprimento da primeira reta: '))
 r2 = float(input('Comprimento da segunda reta: '))
 r3 = float(input('Comprimento da terceira reta: '))
-if (r1 - r2) < r3 < (r1 + r2) and (r2 - r3) < r1 < (r2 + r3) and (
-            r3 - r1) < r3 < (r3 + r1):
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print('Dá pra fazer um triangulo!')
 else:
     print('Não dá pra fazer')
