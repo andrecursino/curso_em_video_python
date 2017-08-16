16e904b5d93491956403f60cc63afb6643930bc9
sequeloble <tecnicaiplace@iPlaces-MacBook-Pro.local>
2017-08-16 15:43:59 -0300

    ano bissexto

--- Desafios/032.py
+++ Desafios/032.py
@@ -1,5 +1,8 @@
-ano = int(input('Insira o ano: '))
-if ano%4 == 0:
+from datetime import date
+ano = int(input('Insira o ano(ou coloque 0 para o ano atual): '))
+if ano == 0:
+    ano = date.today().year #ano da máquina.
+if ano%4 == 0 and ano%100 != 0 or ano%400 == 0:
     print('{} é bissexto.'.format(ano))
 else:
     print('{} não é bissexto.'.format(ano))
