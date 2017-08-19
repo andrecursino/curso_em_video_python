9fdcc252be8bb4b4c825dee51bb1b1f94976fa36
sequeloble <tecnicaiplace@iPlaces-MacBook-Pro.local>
2017-08-19 14:48:53 -0300

    cores

---
+++ python_teste/aula11.py
@@ -0,0 +1,24 @@
+\033[0;33;44m
+'''1o style, 2o texto, 3o fundo
+style
+0 = none
+1 = bold
+4 = underline
+7 = negative
+
+text
+30 - 37
+branco, verm, verde, amarelo, azul,
+roxo, azul claro, cinza
+
+fundo
+40 - 47
+mesmas cores.
+'''
+
+\033[0;30;41m #fundo vermelho, letra branca
+\033[4;33;44m #fundo azul, letra amarela sublin
+\033[1;35;43m #fundo amarelo, letra roxa, negrito
+\033[30;42m #fundo verde, sem estilo, letra branca
+\033[m #padrao
+\033[7;30m #estilo negativo, letra branca
