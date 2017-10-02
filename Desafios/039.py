be9da97ed1d4b1d171ff3067b3b070b3b0f64fe1
sequeloble <andrephelipe@outlook.com>
2017-10-02 18:18:40 -0300

    alistamento

--- Desafios/039.py
+++ Desafios/039.py
@@ -1,9 +1,15 @@
 nasc = int(input('Insira o ano de nascimento(ex.:1999): '))
 alistamento = 2017 - nasc
+falta = alistamento - 18
 
 if alistamento == 18:
     print('Tá na hora de se alistar.')
+    print('Vai!!')
 elif alistamento > 18:
-    print('Passou do tempo.')
+    print('Passou do tempo. Deveria ter feito {} anos atrás.'.format(
+        abs(falta)
+    ))
 elif alistamento < 18:
-    print('Ainda vai se alistar.')
+    print('Ainda vai se alistar. Espere {} anos.'.format(
+        abs(falta)
+    ))
