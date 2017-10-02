2110a54b6374d8330207bed04461c003ef0a34ad
sequeloble <andrephelipe@outlook.com>
2017-10-02 17:51:27 -0300

    maior

---
+++ Desafios/038.py
@@ -0,0 +1,9 @@
+num1 = int(input('Primeiro valor: '))
+num2 = int(input('Segundo valor: '))
+
+if num1 > num2:
+    print('O primeiro valor é maior.')
+elif num2 > num1:
+    print('O segundo número é maior.')
+elif num1 == num2:
+    print('Não existe valor maior, os dois são iguais.')
